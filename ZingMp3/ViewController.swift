72cba0a5263790f10e100a0d8514909c1a4591b4
iOS Student <illdiandh94@gmail.com>
2017-02-16 15:24:21 +0700

    Initial Commit

---
+++ ZingMp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZingMp3
+//
+//  Created by iOS Student on 2/16/17.
+//  Copyright © 2017 NKT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
